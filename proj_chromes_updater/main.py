db044a5a3840d21c118776821c2cce0d7de6a99a
Luiz <luizhtrincac@gmail.com>
2022-06-28 11:22:27 -0300

    Update-28-06-22

--- proj_chromes_updater/main.py
+++ proj_chromes_updater/main.py
@@ -1,19 +1,40 @@
 from tkinter import *
 from tkinter import ttk
-import turtle
 
 
+def update():
+    a = first_entry.get()
+    b = last_entry.get()
+    print(a, b)
+
+
+# Root screen
 root = Tk()
 
-form = ttk.Frame(root, padding = 20, borderwidth = 5)
+# StringVar()
+first_chrome = StringVar(root)
+last_chrome = StringVar(root)
+user = StringVar(root)
+
+# Creating form grid layout
+form = ttk.Frame(root, padding=20, borderwidth=5)
 form.grid()
 
+# Widget Creation
+status_label = ttk.Label(form, text='Status Updater', padding=15)
+first_entry = ttk.Entry(form, textvariable=first_chrome)
+to_label = ttk.Label(form, text='to')
+last_entry = ttk.Entry(form, textvariable=last_chrome)
+user_entry = ttk.Entry(form, textvariable=user)
+update_button = ttk.Button(form, text='Update', command=lambda: update())
 
-ttk.Label(form, text = 'Status Updater', padding = 15).grid(column = 1, row = 0)
-ttk.Entry(form).grid(column = 0, row = 1)
-ttk.Label(form, text = 'to').grid(column = 1, row = 1)
-ttk.Entry(form).grid(column = 2, row = 1)
-ttk.Entry(form).grid(column = 1, row = 2)
-ttk.Button(form, text = 'Update').grid(column = 1, row = 3)
+# Widget Layout
+status_label.grid(column=1, row=0)
+first_entry.grid(column=0, row=1)
+to_label.grid(column=1, row=1)
+last_entry.grid(column=2, row=1)
+user_entry.grid(column=1, row=2)
+update_button.grid(column=1, row=3)
 
+# End of screen
 root.mainloop()
